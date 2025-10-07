5403dfddc41c9d7778830d7c67c28b232c3af479
ruskyvisky <numan_859@hotmail.com>
2025-10-07 19:38:59 +0300

    Refactor createUser function to improve code structure and readability

--- src/services/member.service.js
+++ src/services/member.service.js
@@ -5,3 +5,8 @@ const app = express();
 export const getUserById =  async (req, res, next) => {
     return res.status(200).json({success: true, data: {id: req.params.id, name: "John Doe"}});
 }
+
+export const createUser = async (req, res, next) => {
+    const { name } = req.body;
+    return res.status(201).json({success: true, data: {id: 1, name}});
+}
