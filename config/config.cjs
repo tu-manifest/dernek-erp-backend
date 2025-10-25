4fff0d6bd1daebd97b2489b7eb39abce6976037f
ruskyvisky <numan_859@hotmail.com>
2025-10-25 15:54:02 +0300

    feat: remove SSL dialect options from production database configuration

--- config/config.cjs
+++ config/config.cjs
@@ -26,12 +26,7 @@ module.exports = {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     dialect: 'postgres',
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false
-      }
-    },
+
     logging: false
   }
 };
